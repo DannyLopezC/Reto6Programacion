812c3711258161ec922ea178a2b7cd554f6c466f
Danny López Cárdenas <danny.lopezc00@gmail.com>
2025-06-02 17:13:02 -0500

    Flowcharts

--- Scripts/punto_2.py
+++ Scripts/punto_2.py
@@ -1,12 +1,12 @@
 if __name__ == "__main__":
     i = 0
-    while i < 999:
+    while i < 1000:
         i += 1
         if i % 2 != 0:
             print(i)
 
     i = 0
-    while i < 999:
+    while i < 1000:
         i += 1
         if i % 2 == 0:
             print(i)
